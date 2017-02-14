961f5aa7f76d3ba33413507e36df2c99341d5cea
Nima Baghdadi <bg.nima@gmail.com>
2017-02-14 22:25:11 +0330

    implemented excel to json func

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # x2j2x
+Converting json file to excel and vice versa using nodejs.
+
+Supporting json standards like nested objects and arrays. It converts json file to a two column excel file by which the first column will contain a flat dot seperated path to each value and the second column will contain the value. Later on you can convert the excel file agian to the same json structure using the reverse function.
+
+## Install
+```
+npm install x2j2x
+```
+
+## Usages
+I use it when I have provided a json file for my multiple language applications, containing keys and values in a base language and I want to hand in them to some translator (mashine/human) and when I have translations (new values) in a diffrent column I can retransform it to json format using the existing key and new value columns.
+But you can use it for whatever you like :)
+
+```javascript
+var x2j2x = require('x2j2x'); 
+// for converting an excel file to json object
+var json_object = x2j2x.excel_to_json('input.xlsx');
+// for converting a json_object to excel file
+x2j2x.json_to_excel(json_object, 'out.xlsx');
+```
+
+## Docs
+So obvious!
+
+```javascript
+/*
+ * object: any - a javascript/json object to be converted to excel file
+ * outPath: string - excel file path to save on
+ * sheetName: string - save data on this sheetNane (by default 'Sheet1')
+**/
+json_to_excel(object, outPath, sheetName = 'Sheet1')
+/*
+ * inputPath: string - excel file path to read key and values form
+ * sheetName: string - save data on this sheetNane (by default 'Sheet1')
+ * keyColNum: numeber - column number on which keys are stored (by default 0)
+ * valColNum: numeber - column number on which values are stored (by default 1)
+**/
+excel_to_json(inputPath, sheetName='Sheet1', keyColNum=0, valColNum=1)
+```
---
+++ x2j2x.js
@@ -0,0 +1,133 @@
+
+var x2j2x = {};
+(function make_nwise(x2j2x) {
+
+    var XLSX = require('xlsx');
+    var XLSX_EXTEND = require('./xlsx.utils');
+
+    function join_object_path(p1, p2) {
+        if (!p1) {
+            return p2;
+        }
+        if (!p2) {
+            return p1;
+        }
+        return `${p1}.${p2}`;
+    }
+
+    function flatten_object(jsonData, basePath, output) {
+        var output = output || [];
+        basePath = basePath || '';
+        if (!Object.isExtensible(jsonData)) {
+            output.push([basePath, jsonData]);
+            return output;
+        }
+        for (var key in jsonData) {
+            var value = jsonData[key];
+            if (Object.isExtensible(value)) {
+                if (!Array.isArray(value)) {
+                    flatten_object(value, join_object_path(basePath, key), output);
+                } else {
+                    value.forEach((item, i) => {
+                        flatten_object(item, join_object_path(basePath, key) + `[${i}]`, output);
+                    });
+                }
+            } else {
+                output.push([join_object_path(basePath, key), value]);
+            }
+        }
+
+        return output;
+    }
+
+    function object_array_to_sheet(data, opts) {
+        var ws = {};
+        var range = { s: { c: 10000000, r: 10000000 }, e: { c: 0, r: 0 } };
+        for (var R = 0; R != data.length; ++R) {
+            for (var C = 0; C != data[R].length; ++C) {
+                if (range.s.r > R) range.s.r = R;
+                if (range.s.c > C) range.s.c = C;
+                if (range.e.r < R) range.e.r = R;
+                if (range.e.c < C) range.e.c = C;
+                var cell = { v: data[R][C] };
+                if (cell.v == null) continue;
+                var cell_ref = XLSX.utils.encode_cell({ c: C, r: R });
+
+                if (typeof cell.v === 'number') cell.t = 'n';
+                else if (typeof cell.v === 'boolean') cell.t = 'b';
+                else if (cell.v instanceof Date) {
+                    cell.t = 'n'; cell.z = XLSX.SSF._table[14];
+                    cell.v = datenum(cell.v);
+                }
+                else cell.t = 's';
+
+                ws[cell_ref] = cell;
+            }
+        }
+        if (range.s.c < 10000000) ws['!ref'] = XLSX.utils.encode_range(range);
+        ws['!merges'] = [];
+        return ws;
+    }
+
+    function create_empty_workbook() {
+        return {
+            SheetNames: [],
+            Sheets: {}
+        }
+    }
+
+    /*
+    * inputPath: string - excel file path to read key and values form
+    * sheetName: string - save data on this sheetNane (by default 'Sheet1')
+    * keyColNum: numeber - column number on which keys are stored (by default 0)
+    * valColNum: numeber - column number on which values are stored (by default 1)
+    **/
+    function json_to_excel(object, outPath, sheetName = 'Sheet1') {
+        var flattenObj = flatten_object(object);
+        var workbook = create_empty_workbook();
+        var worksheet = object_array_to_sheet(flattenObj);
+
+        workbook.SheetNames.push(sheetName);
+        workbook.Sheets[sheetName] = worksheet;
+        XLSX.writeFile(workbook, outPath);
+    }
+
+    /*
+    * object: any - a javascript/json object to be converted to excel file
+    * outPath: string - excel file path to save on
+    * sheetName: string - save data on this sheetNane (by default 'Sheet1')
+    **/
+    function extend_object(keyParts, value, object) {
+        if (!keyParts.length) return;
+        var firstPart = keyParts[0];
+        var index;
+        if (index = /\[\d\]$/.exec(firstPart)){
+            firstPart = firstPart.replace(index,"");
+            index = parseInt(/\d/.exec(index[0])[0]);
+            object[firstPart] = object[firstPart]||[];
+            object[firstPart][index] = keyParts.length==1 ? value : (object[firstPart][index]||{});
+            extend_object(keyParts.splice(1), value, object[firstPart][index]);
+        } else {
+            object[firstPart] = keyParts.length==1 ? value : (object[firstPart]||{});
+            extend_object(keyParts.splice(1), value, object[firstPart]);
+        }
+    }
+
+    function excel_to_json(inputPath, sheetName='Sheet1', keyColNum=0, valColNum=1) {
+        var workbook = XLSX.readFile(inputPath);
+        var worksheet = workbook.Sheets[sheetName];
+        var objArray = XLSX.utils.sheet_to_row_object_array(worksheet, { header: 1 });
+        var finalObject = {};
+        for (var i = 0; i < objArray.length; i++) {
+            var key = objArray[i][keyColNum];
+            var value = objArray[i][valColNum];
+            var keyParts = key.split('.');
+            extend_object(keyParts, value, finalObject)
+        }
+        return finalObject;
+    }
+
+    x2j2x.json_to_excel = json_to_excel;
+    x2j2x.excel_to_json = excel_to_json;
+
+})(typeof exports !== 'undefined' ? exports : x2j2x);
